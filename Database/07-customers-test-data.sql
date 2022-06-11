4f1b893e6892a0efcf8d256062975324fdab6e1b
cemalaydeniz <c.a.aydeniz@gmail.com>
2022-06-12 02:02:53 +0300

    Added test data to database

---
+++ Database/07-customers-test-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `customer` (`Name`,`Contact`)
+VALUES
+	("Portia Sweet","1-267-858-1227"),
+	("Leila Curtis",null),
+	("Cleo Wells","1-867-823-4246"),
+	("Reuben Bass",null),
+	("Dieter Bates",null),
+	("Hu Ryan","(311) 575-0326"),
+	("Dorian Avery","(429) 820-6572"),
+	("Jasper Mclaughlin",null),
+	("Aidan Sheppard","(934) 389-8268"),
+	("Noah Bolton","(862) 481-5875")
