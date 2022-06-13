08500b818ca18708aa67857b34c3c1da423dd083
cemalaydeniz <c.a.aydeniz@gmail.com>
2022-06-13 16:29:53 +0300

    updated Create Service Command
    A connection between New Service and Edit Service Windows has been added. The New Service window now send the entered license plate in order to show the related vehicle's data in the Edit Service Window

--- Mechanic/Commands/NewService/CreateServiceCommand.cs
+++ Mechanic/Commands/NewService/CreateServiceCommand.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
 
 using Mechanic.ViewModels;
@@ -38,14 +33,26 @@ namespace Mechanic.Commands.NewService
 
         public override void Execute(object? parameter)
         {
-            if (parameter is not NewServiceWindow)
+            // Parameters check to see if they are correct
+            if (parameter is not object[])
                 return;
 
+            object[] parameters = (object[])parameter;
+            if (parameters.Length != 2)
+                return;
+
+            NewServiceWindow newServiceWindow = (NewServiceWindow)parameters[0];
+            string licensePlate = (string)parameters[1];
+            if (newServiceWindow == null || licensePlate == null)
+                return;
+
+            // Create a Edit Service Window and fill the form if the vehicle already exists
             EditServiceWindow editWindow = new EditServiceWindow();
             editWindow.Owner = Application.Current.MainWindow;
+            ((EditServiceViewModel)editWindow.DataContext).FillForm(licensePlate);
             editWindow.Show();
 
-            ((NewServiceWindow)parameter).Close();
+            newServiceWindow.Close();
         }
     }
 }
---
+++ Mechanic/ViewModels/EditServiceViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mechanic.ViewModels
+{
+    public class EditServiceViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Fills the text fields if a vehicle with the license place exists
+        /// </summary>
+        /// <param name="licensePlate">The license plate of the vehicle</param>
+        public void FillForm(string licensePlate)
+        {
+
+        }
+    }
+}
